d9b355d6931a124e814f3c0e2b11bba24cb17906
Uzalov-Gamid <uzalovgamid@gmail.com>
2026-03-27 21:28:41 +0300

    Init: Start the project

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, opts = { contrast = "hard" } },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
